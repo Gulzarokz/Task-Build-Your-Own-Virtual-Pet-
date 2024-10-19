cb1a7170cca47490c86b9a9d0d8eb891d053ef5f
Gulzarokz <gokz22301@gamil.com>
2024-10-19 21:33:19 +0500

    Done user input for different methods and property

---
+++ index.js
@@ -0,0 +1,20 @@
+// Task to build Task-Build-Your-Own-Virtual-Pet
+
+   var name = prompt("What is your pet Name!");
+    alert(name);
+    
+    var type = prompt("What kind of pet is it (e.g dog cat dragon)");
+    alert(type);
+    
+    var age = prompt("How old is your pet!");
+    alert(age);
+    
+    var pethappiness = prompt("How much is your cat happy!");
+    alert(pethappiness);
+    
+    var hunger = prompt("How hungry you pet!");
+    alert(hunger);
+    
+   var userInput = prompt("What would you like to do with sazi \n1-feed\n2-play\n3-age\n4-quit");
+    alert(userInput);
+
